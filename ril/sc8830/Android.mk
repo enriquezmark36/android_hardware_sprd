d44aa489ae56526752ffc5a7e79af3c41dbce3ec
Mark Enriquez <enriquezmark36@gmail.com>
2020-04-11 11:12:46 +0800

    ril: kanas: Use the scx15 version
    We don't even use the modem_control daemon, even from the stock rom.
    
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- ril/sc8830/Android.mk
+++ ril/sc8830/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),sc8830)
+ifneq ($(SOC_SCX35),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
 endif
--- ril/scx15/Android.mk
+++ ril/scx15/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_BOARD_PLATFORM),scx15)
+ifneq ($(filter $(TARGET_BOARD_PLATFORM), scx15) $(filter true, $(SOC_SCX35)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+
+
